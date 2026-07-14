b773f2718303019f223e1f85635be082575b2a14
siamakgh59 <77428575+siamakgh59@users.noreply.github.com>
2026-07-15 00:13:02 +0330

    Update yahoo.py

--- app/data/providers/yahoo.py
+++ app/data/providers/yahoo.py
@@ -8,50 +8,64 @@ class YahooFinanceProvider(BaseProvider):
 
     name = "Yahoo Finance"
 
+
     def get_price(self, symbol: str):
 
         try:
+
             ticker = yf.Ticker(symbol)
 
             data = ticker.history(
-                period="1d",
-                interval="1m"
+                period="3mo",
+                interval="1d"
             )
 
+
             if data.empty:
+
                 return ProviderResponse(
+                    provider=self.name,
                     symbol=symbol,
                     price=0,
                     change=0,
-                    provider=self.name
+                    success=False
                 )
+
 
             last_price = float(
                 data["Close"].iloc[-1]
             )
 
+
             first_price = float(
-                data["Close"].iloc[0]
+                data["Close"].iloc[-2]
             )
 
+
             change = (
-                (last_price - first_price)
-                / first_price
-            ) * 100 if first_price else 0
+                (last_price-first_price)
+                /
+                first_price
+            ) * 100
+
 
             return ProviderResponse(
-    symbol=symbol,
-    price=round(last_price, 4),
-    change=round(change, 2),
-    provider=self.name,
-    success=True
-)
+                provider=self.name,
+                symbol=symbol,
+                price=round(last_price,4),
+                change=round(change,2),
+                success=True,
+                history=data["Close"].tolist()
+            )
+
 
         except Exception:
 
+
             return ProviderResponse(
+                provider=self.name,
                 symbol=symbol,
                 price=0,
                 change=0,
-                provider=self.name
+                success=False
             )
