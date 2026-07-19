c607c23ff0fb0b6d45a132b8550150c5b44ee143
siamakgh59 <77428575+siamakgh59@users.noreply.github.com>
2026-07-19 13:35:42 +0330

    Add risk factors constants to factors.py

---
+++ app/engines/risk/factors.py
@@ -0,0 +1,11 @@
+MARKET_RISK = "market_risk"
+
+VOLATILITY_RISK = "volatility_risk"
+
+MACRO_RISK = "macro_risk"
+
+LIQUIDITY_RISK = "liquidity_risk"
+
+GEOPOLITICAL_RISK = "geopolitical_risk"
+
+DATA_CONFIDENCE_RISK = "data_confidence_risk"
