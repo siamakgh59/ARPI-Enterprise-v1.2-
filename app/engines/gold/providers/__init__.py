eb00b74ffe02eb5c1ffa710b26760ec5be1e1895
siamakgh59 <77428575+siamakgh59@users.noreply.github.com>
2026-07-27 00:41:07 +0330

    Update __init__.py

--- app/engines/gold/providers/__init__.py
+++ app/engines/gold/providers/__init__.py
@@ -1,4 +1,4 @@
-from .faraz_gold_provider import FarazGoldProvider
+from .faraz_provider import FarazGoldProvider
 from .faraz_parser import FarazParser
 from .coin_parser import CoinParser
 from .normalizer import GoldNormalizer
