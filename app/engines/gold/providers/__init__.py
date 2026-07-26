5dcff7a958ded494b7d9001ab4f9e89465a23818
siamakgh59 <77428575+siamakgh59@users.noreply.github.com>
2026-07-27 01:32:55 +0330

    Add GlobalGoldProvider to exports

--- app/engines/gold/providers/__init__.py
+++ app/engines/gold/providers/__init__.py
@@ -1,4 +1,5 @@
 from .faraz_provider import FarazGoldProvider
+from .global_gold_provider import GlobalGoldProvider
 from .faraz_parser import FarazParser
 from .coin_parser import CoinParser
 from .normalizer import GoldNormalizer
@@ -6,6 +7,7 @@ from .normalizer import GoldNormalizer
 
 __all__ = [
     "FarazGoldProvider",
+    "GlobalGoldProvider",
     "FarazParser",
     "CoinParser",
     "GoldNormalizer",
